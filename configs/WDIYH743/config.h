a7ea1077e7a64dd9bb9f703c5f09560252ff1a53
Mark Haslinghuis <mark@numloq.nl>
2025-03-01 14:24:07 +0100

    Update dma for PC6 (PPM)

--- configs/WDIYH743/config.h
+++ configs/WDIYH743/config.h
@@ -115,13 +115,13 @@
     TIMER_PIN_MAP( 6, PB0,  2,  5) \
     TIMER_PIN_MAP( 7, PB1,  2,  6) \
     TIMER_PIN_MAP( 8, PB8,  2,  7) \
-    TIMER_PIN_MAP( 9, PC6,  3,  0) \
+    TIMER_PIN_MAP( 9, PC6,  3,  8) \
     TIMER_PIN_MAP(10, PE11, 1, -1) \
     TIMER_PIN_MAP(11, PE14, 1, -1)
   
 #define ADC1_DMA_OPT        9
 #define ADC2_DMA_OPT        10
-#define ADC3_DMA_OPT       11
+#define ADC3_DMA_OPT        11
  
 #define TIMUP1_DMA_OPT      0
 #define TIMUP3_DMA_OPT      0
